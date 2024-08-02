4497ca32364defe304d8be0f10f801ad13a288cb
crocsg <pub.sg@free.fr>
2024-08-02 16:48:16 +0000

    fix

--- build_desktopbraillerap.sh
+++ build_desktopbraillerap.sh
@@ -5,9 +5,13 @@ export DISPLAY
 python3 -m venv venv
 source ./venv/bin/activate
 
-printf "\e[1;34m######################\e[0m\n"
+# checkout last revision
+git pull
+git checkout $BRANCH_BUILD 
+
+printf "\e[1;34m###########################\e[0m\n"
 printf "\e[1;34minstall python dependencies\e[0m\n" 
-printf "\e[1;34m######################\e[0m\n"
+printf "\e[1;34m###########################\e[0m\n"
 pip install -r /home/builduser/DesktopBrailleRAP/requirement_linux.txt
 
 printf "\e[1;34m######################\e[0m\n"
@@ -18,18 +22,25 @@ printf "nodejs :%s\n" $(node --version)
 printf "npm    :%s\n" $(npm --version)
 printf "branch :%s\n" "$BRANCH_BUILD"
 
-
-npm install
-
 rm -r /home/builduser/dist/*
 
 printf "writing python linux dependencies\n" 
 pip freeze > /home/builduser/dist/requirement_test.txt
 
-git pull
-git checkout $BRANCH_BUILD 
+
+# !! delete .gitignore !!
+ls -lah /home/builduser/DesktopBrailleRAP/package/ubuntu/desktopbraillerap-ubuntu/bin/.*
+rm /home/builduser/DesktopBrailleRAP/package/ubuntu/desktopbraillerap-ubuntu/bin/.*
 
 tree -L 4 ./package
+
+
+
+#install nodejs depencies
+printf "\e[1;34m###########################\e[0m\n"
+printf "\e[1;34minstall nodejs dependencies\e[0m\n" 
+printf "\e[1;34m###########################\e[0m\n"
+npm install
 
 #printf "\e[1;34mBuild debug \e[0m\n"
 #npm run builddev
